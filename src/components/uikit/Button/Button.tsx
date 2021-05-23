a329c2568c04db243eff5210af1d38bd682f24e6
goraychiy13 <gor.vya4eslav@yandex.ru>
2021-05-23 23:38:01 +0300

    replace styles props to styleProps

--- src/components/uikit/Button/Button.tsx
+++ src/components/uikit/Button/Button.tsx
@@ -1,59 +1,44 @@
-import { TypeIcon } from '../../../icons/types';
 import { memo } from 'react';
 import Icon from '../Icon';
 import { StyledButton } from './style';
+import { SyleProps } from './type';
 
 type Props = {
   onClick?: void;
-  height?: string;
-  width?: string;
-  maxWidth?: string;
-  margin?: string;
-  padding?: string;
-  backgroundColor: string;
-  borderRadius?: string;
-  colorModifier?: string;
-  textDecoration?: string;
-  fontWeight?: string;
-  color?: string;
-  hasIcon?: boolean;
-  children?: string;
-  iconWidth?: string;
-  iconHeight?: string;
-  iconMargin?: string;
-  iconFill?: string;
-  isRowReversed?: boolean;
-  icon?: TypeIcon;
-  type?: string;
 };
 
-function Button(props: Props): JSX.Element {
+function Button(props: Props & SyleProps): JSX.Element {
+  const buttonStyleProps = {
+    height: props.height,
+    width: props.width,
+    maxWidth: props.maxWidth,
+    margin: props.margin,
+    padding: props.padding,
+    backgroundColor: props.backgroundColor,
+    colorModifier: props.colorModifier,
+    color: props.color,
+    textDecoration: props.textDecoration,
+    fontWeight: props.fontWeight,
+    borderRadius: props.borderRadius,
+    isRowReversed: props.isRowReversed,
+    type: props.type,
+  };
+
+  const iconStyleProps = {
+    width: props.iconWidth,
+    height: props.iconHeight,
+    margin: props.iconMargin,
+    iconFill: props.iconFill,
+  };
+
   return (
-    <StyledButton
-      data-name="styled-button"
-      height={props.height}
-      width={props.width}
-      maxWidth={props.maxWidth}
-      margin={props.margin}
-      padding={props.padding}
-      backgroundColor={props.backgroundColor}
-      colorModifier={props.colorModifier}
-      color={props.color}
-      textDecoration={props.textDecoration}
-      fontWeight={props.fontWeight}
-      borderRadius={props.borderRadius}
-      isRowReversed={props.isRowReversed}
-      type={props.type}
-    >
+    <StyledButton data-name="styled-button" {...buttonStyleProps}>
       {props.hasIcon && props.icon && (
         <Icon
           title="Button"
           path={props.icon.path}
           viewBox={props.icon.viewBox}
-          width={props.iconWidth}
-          height={props.iconHeight}
-          margin={props.iconMargin}
-          iconFill={props.iconFill}
+          {...iconStyleProps}
         />
       )}
       <span>{props.children}</span>
---
+++ src/components/uikit/Button/type.ts
@@ -0,0 +1,24 @@
+import { TypeIcon } from '../../../icons/types';
+
+export type SyleProps = {
+  height?: string;
+  width?: string;
+  maxWidth?: string;
+  margin?: string;
+  padding?: string;
+  backgroundColor: string;
+  borderRadius?: string;
+  colorModifier?: string;
+  textDecoration?: string;
+  fontWeight?: string;
+  color?: string;
+  hasIcon?: boolean;
+  children?: string;
+  iconWidth?: string;
+  iconHeight?: string;
+  iconMargin?: string;
+  iconFill?: string;
+  isRowReversed?: boolean;
+  icon?: TypeIcon;
+  type?: string;
+}
